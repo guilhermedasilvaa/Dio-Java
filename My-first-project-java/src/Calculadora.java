44310f228e4f797a812c66a51c963e321bb8a2c9
Guilherme <guilhermeds.gru@gmail.com>
2025-03-11 18:52:54 -0300

    commit full

---
+++ My-first-project-java/src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora{
+    /**
+     * <h1>Calculadora</h1>
+     *A calculadora relaziza operações maetematicas entre numeros inteiros
+     *<p>
+     *<b>Note:</b> Leira atentamente a documentação desta classe para poder desfrutar de 
+     *todos os recursos craidos pelo autor
+     </p>
+     * @autor Guilherme da silva
+     * @Version 1.0
+     * @Since 09/03/2025
+     */
+    public int somar(int numeroUm, int numeroDois)
+    {
+        /** 
+         * Este é o metodo utilizado para somar dois    numeros inteiros
+         * @param numeroUm este é o primeiro parametro para a soma
+         * @param numeroDois este é o segundo parametro para soma
+         * @return int retorna os dois parametros somados
+         */
+        return numeroUm + numeroDois;
+    }
+
+}
